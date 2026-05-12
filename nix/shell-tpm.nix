f4c6472cfcd7adace03d3875c15821f9c58a99a7
Cameron Dugan <camdugan@amazon.com>
2026-05-12 12:50:48 -0400

    ci(nix): pin nixpkgs to commit with Go 1.26.2

--- nix/shell-tpm.nix
+++ nix/shell-tpm.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchTarball {
+    # nixpkgs with Go 1.26.2
+    url = "https://github.com/NixOS/nixpkgs/archive/01fbdeef22b76df85ea168fbfe1bfd9e63681b30.tar.gz";
+  }) {} }:
 
 let
   base = import ./shell-base.nix { inherit pkgs; };
